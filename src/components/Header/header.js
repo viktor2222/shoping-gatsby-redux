ecb5cc39e77a2a98610487c00d6270b8b4804900
v.panchuk <v.panchuk@pozitiff.ua>
2019-07-10 20:27:38 +0300

    add header component

---
+++ src/components/Header/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { HeaderContainer, ImgCart } from './styled'
+
+const Header = () => (
+  <HeaderContainer>
+    <ImgCart src='./cart-icon.png' alt='cart' />
+  </HeaderContainer>
+)
+
+export default Header
---
+++ src/components/Header/styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.div`
+    background-color: #1e1e1e;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 15px 10px;
+`;
+
+export const ImgCart = styled.img`
+    height: 30px;
+`;
