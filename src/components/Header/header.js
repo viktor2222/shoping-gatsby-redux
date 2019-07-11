10eaf9bd36601b1d15496cdd025a17501ac2ea6d
v.panchuk <v.panchuk@pozitiff.ua>
2019-07-11 12:34:58 +0300

    fix

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -20,9 +20,6 @@ const Header = () => {
             <li>1</li>
             <li>2</li>
             <li>3</li>
-            <li>4</li>
-            <li>5</li>
-            <li>6</li>
           </CartList>
           )
         }
