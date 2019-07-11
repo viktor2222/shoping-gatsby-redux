84117049e70f6d67019f52e4d5b2d005b7851dce
v.panchuk <v.panchuk@pozitiff.ua>
2019-07-11 11:59:52 +0300

    add cart

--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -11,3 +11,29 @@ export const HeaderContainer = styled.div`
 export const ImgCart = styled.img`
     height: 30px;
 `;
+
+export const CartList = styled.ul`
+    position: absolute;
+    background-color: #999;
+    padding: 15px;
+`;
+
+export const ActiveList = styled.div`
+    position: relative;
+    cursor: pointer;
+`;
+
+export const CartCount = styled.span`
+    position: absolute;
+    height: 20px;
+    width: 20px;
+    border-radius: 50%;
+    background-color: blue;
+    font-size: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    top: -5px;
+    right: -17px;
+    color: #fff;
+`;
