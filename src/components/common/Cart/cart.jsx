a96a191dfd51e529a929169e45170ed25fb4f9cd
v.panchuk <v.panchuk@pozitiff.ua>
2019-07-23 18:21:43 +0300

    add custom style to cart modal

--- src/components/common/Cart/cart.jsx
+++ src/components/common/Cart/cart.jsx
@@ -7,9 +7,24 @@ import {
   SumCount, CloseButton,
 } from './styled'
 
+const customStyles = {
+  content: {
+    top: '50%',
+    left: 'auto',
+    right: '0',
+    bottom: 'auto',
+    height: '100vh',
+    borderRadius: 'none',
+    maxWidth: '350px',
+    width: '100%',
+    transform: 'translateY(-50%)',
+  },
+}
+
 export const Cart = inject('cartStore')(observer(({ cartStore }) => (
   <CustomModal
     isOpen={cartStore.show}
+    cartModal={customStyles}
   >
     <CloseButton onClick={() => cartStore.hideCart()} />
     <CartProductItem />
--- src/components/common/Modal/modal.jsx
+++ src/components/common/Modal/modal.jsx
@@ -3,26 +3,11 @@ import Modal from 'react-modal'
 
 Modal.setAppElement('#___gatsby')
 
-const customStyles = {
-  content: {
-    top: '50%',
-    left: 'auto',
-    right: '0',
-    bottom: 'auto',
-    height: '100vh',
-    borderRadius: 'none',
-    maxWidth: '350px',
-    width: '100%',
-    transform: 'translateY(-50%)',
-  },
-}
-
-
-export const CustomModal = ({ isOpen, children }) => (
+export const CustomModal = ({ isOpen, cartModal, children }) => (
   <>
     <Modal
       isOpen={isOpen}
-      style={customStyles}
+      style={cartModal}
     >
       { children }
     </Modal>
